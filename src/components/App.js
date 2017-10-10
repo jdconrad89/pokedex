291f9cb03fe399628fe06272ce0c66cbbc31501b
Jason Conrad <jason.d.conrad.89@gmail.com>
2017-10-10 14:43:13 -0600

    add images for pokedex as well as create the PokeCells to hold the images for the pokemon images to live in

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PokeList from './PokeList';
 import './styles/App.css';
 
 class App extends Component {
@@ -10,7 +11,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <PokeList />
       </div>
     );
   }
