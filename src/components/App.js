dc3412ec6608c1392de7fd5b889502877fd0f0f2
Jason Conrad <jason.d.conrad.89@gmail.com>
2017-10-11 15:31:54 -0600

    pokedex is hitting the poke api to access the information for pokemon, and details are showing in the detail view

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,42 @@
 import React, { Component } from 'react';
 import PokeList from './PokeList';
 import DetailView from './DetailView';
+import Pokemon from '../Pokemon';
 import './styles/App.css';
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      pokemon: {}
+    };
+
+    this.handleOnClick = this.handleOnClick.bind(this);
   }
+
+  handleOnClick(id) {
+    fetch(`http://pokeapi.co/api/v2/pokemon/${id}/`)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        const pokemon = new Pokemon(data);
+
+        this.setState({ pokemon });
+      })
+      .catch(err => console.log(err));
+  }
+
+
 
   render() {
     return (
       <div className="App">
-        <PokeList />
-        <DetailView />
+        <PokeList handleOnClick={this.handleOnClick} />
+        <DetailView pokemon={this.state.pokemon} />
       </div>
     );
   }
 }
+
 
 export default App;
--- src/components/DetailView.js
+++ src/components/DetailView.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './styles/DetailView.css';
 
-const DetailView = () => {
+const DetailView = ({ pokemon }) => {
+  const { id, name, sprite, type } = pokemon;
 
   return (
     <section className="detail-view">
-      <img className='sprite-image' />
+      <img src={sprite} className='sprite-image' alt="sprite"/>
       <div className='data-wrapper'>
-        <h1 className='data-name'></h1>
-        <p className="data-char"></p>
+        <h3 className='data-name'>ID: {id}</h3>
+        <h3 className='data-name'>Name: {name}</h3>
+        <p className="data-char">Type: {type}</p>
       </div>
     </section>
   )
